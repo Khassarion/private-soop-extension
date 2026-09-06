3fc0c12e4464c77140f86dbeec49f9323e595ce7
AINukeHere <iii4625@naver.com>
2026-09-06 17:21:41 +0900

    play 페이지 자동 재생 끄기 추가

---
+++ src/disableAutoPlay.js
@@ -0,0 +1,17 @@
+(() => {
+  const autoPlayButtonSelector = '#player > div.message_player_wrap > div > button';
+  const clickedButtons = new WeakSet();
+
+  const disableAutoPlay = () => {
+    const autoPlayButton = document.querySelector(autoPlayButtonSelector);
+    if (!autoPlayButton || clickedButtons.has(autoPlayButton)) return;
+
+    clickedButtons.add(autoPlayButton);
+    autoPlayButton.click();
+  };
+
+  disableAutoPlay();
+
+  const observer = new MutationObserver(disableAutoPlay);
+  observer.observe(document.documentElement, { childList: true, subtree: true });
+})();
